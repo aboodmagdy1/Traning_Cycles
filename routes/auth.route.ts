135b76a6b9f5f9da6c714ea0d395f0b7380d4aea
Abood Magdy <c.aboodmagdy@gmail.com>
2024-03-15 09:21:53 +0200

    feat:add limiter on reset code requests

--- routes/auth.route.ts
+++ routes/auth.route.ts
@@ -1,17 +1,46 @@
 import express from "express";
-import {validationMiddleware} from "../middlewares/validation.middleware";
-import {ForgetPasswordDto, LogInDto, ResetPasswordDto, SignUpDto,VerifyCodeDto} from "../dtos/auth.dto";
-import {logIn, signUp ,forgetPassword,verifyResetCode, resetPassword} from "../controllers/auth.controller";
+import rateLimit from "express-rate-limit";
 
-export const authRouter = express.Router()
+import { validationMiddleware } from "../middlewares/validation.middleware";
+import {
+  ForgetPasswordDto,
+  LogInDto,
+  ResetPasswordDto,
+  SignUpDto,
+  VerifyCodeDto,
+} from "../dtos/auth.dto";
+import {
+  logIn,
+  signUp,
+  forgetPassword,
+  verifyResetCode,
+  resetPassword,
+} from "../controllers/auth.controller";
 
-authRouter.post('/signup',validationMiddleware(SignUpDto),signUp)
-authRouter.post('/login',validationMiddleware(LogInDto),logIn)
-authRouter.post('/password/forget',validationMiddleware(ForgetPasswordDto),forgetPassword)
-authRouter.post('/password/verifyCode',validationMiddleware(VerifyCodeDto),verifyResetCode)
-authRouter.post('/password/reset',validationMiddleware(ResetPasswordDto),resetPassword)
+export const authRouter = express.Router();
 
+const limiter = rateLimit({
+  windowMs: 10 * 60 * 1000,
+  max: 3,
+  message: "Too many requests from this IP, please try again later. ",
+});
 
-
-
-
+authRouter.post("/signup", validationMiddleware(SignUpDto), signUp);
+authRouter.post("/login", validationMiddleware(LogInDto), logIn);
+authRouter.post(
+  "/password/forget",
+  limiter,
+  validationMiddleware(ForgetPasswordDto),
+  forgetPassword
+);
+authRouter.post(
+  "/password/verifyCode",
+  limiter,
+  validationMiddleware(VerifyCodeDto),
+  verifyResetCode
+);
+authRouter.post(
+  "/password/reset",
+  validationMiddleware(ResetPasswordDto),
+  resetPassword
+);
