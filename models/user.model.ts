9da53d3c5e4a04fb053c852729edbfc5406d6e67
Abood Magdy <c.aboodmagdy@gmail.com>
2024-03-15 08:52:12 +0200

    refactor:make hashing pass form model not service

--- models/user.model.ts
+++ models/user.model.ts
@@ -1,4 +1,5 @@
 import mongoose, { Document } from "mongoose";
+import bcrypt from "bcrypt";
 
 const UserSchema = new mongoose.Schema(
   {
@@ -21,5 +22,12 @@ const UserSchema = new mongoose.Schema(
     },
   }
 );
+
+UserSchema.pre('save',async function (next){
+  if(!this.isModified('password')) return next();
+  const salt = await bcrypt.genSalt(10)
+  this.password = await bcrypt.hash(this.password, salt)
+  next();
+})
 
 export const User = mongoose.model("User", UserSchema);
